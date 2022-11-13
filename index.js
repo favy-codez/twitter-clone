68138166323f8cca6e43d39db37ba5e98001da9c
favy-codez <107569845+favy-codez@users.noreply.github.com>
2022-11-14 00:07:50 +0100

    commit message

--- index.js
+++ index.js
@@ -7,45 +7,61 @@ tweetBtn.addEventListener('click', function(){
     console.log(tweetInput.value)
 })
 
-function getFeedHtml(){
-          
-        let feedHtml = ``
+// so we want to add an event listener to listen for clicks on the document,
+document.addEventListener('click', function(e){
+    // so we wan to log out the content of the data-like attribute, .data-set to see what element the data has
+    // if the element we are clicking on has a data attribute called like, if we click on anything that is not like, it  returns undefined
+    if(e.target.dataset.like)
+    handleLikeClick(e.target.dataset.like)
+})
 
-        tweetsData.forEach(function(tweet){
-            feedHtml += `
-    <div class="tweet">
-        <div class="tweet-inner">
-            <img src="${tweet.profilePic}" class="profile-pic">
-            <div>
-                <p class="handle">${tweet.handle}</p>
-                <p class="tweet-text">${tweet.tweetText}</p>
-                <div class="tweet-details">
-                    <span class="tweet-detail">
-                    <i class="fa-solid fa-comment"></i>
-                        ${tweet.replies.length}
-                    </span>
-                    <span class="tweet-detail">
-                    <i class="fa-solid fa-heart"></i>
-                        ${tweet.likes}
-                    </span>
-                    <span class="tweet-detail">
-                    <i class="fa-solid fa-retweet"></i>
-                        ${tweet.retweets}
-                    </span>
-                </div>   
-            </div>            
-        </div>
-    </div>
-    `
-       })
-       return feedHtml 
-    }
+function handleLikeClick(tweetId){
+    console.log(tweetId)
+}
+
+function getFeedHtml(){
+    let feedHtml = ``
     
-    function render(){
-        const feed = document.getElementById('feed').innerHTML = getFeedHtml()
-        // or
+    tweetsData.forEach(function(tweet){
+        feedHtml += `
+<div class="tweet">
+    <div class="tweet-inner">
+        <img src="${tweet.profilePic}" class="profile-pic">
+        <div>
+            <p class="handle">${tweet.handle}</p>
+            <p class="tweet-text">${tweet.tweetText}</p>
+            <div class="tweet-details">
+                <span class="tweet-detail">
+                    <i class="fa-regular fa-comment-dots"
+                    data-reply="${tweet.uuid}"
+                    ></i>
+                    ${tweet.replies.length}
+                </span>
+                <span class="tweet-detail">
+                    <i class="fa-solid fa-heart"
+                    data-like="${tweet.uuid}"
+                    ></i>
+                    ${tweet.likes}
+                </span>
+                <span class="tweet-detail">
+                    <i class="fa-solid fa-retweet"
+                    data-retweet="${tweet.uuid}"
+                    ></i>
+                    ${tweet.retweets}
+                </span>
+            </div>   
+        </div>            
+    </div>
+</div>
+`
+   })
+   return feedHtml 
+}
+// or
         // const feed = document.getElementById('feed')
         // feed.innerHTML = getFeedHtml()
-    }
+function render(){
+    document.getElementById('feed').innerHTML = getFeedHtml()
+}
 
-    render()
+render()
